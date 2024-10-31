acf090b0e2614cd0844e8ceb2564d7d5e4c57f5f
Pablo Brito <pablobrith@gmail.com>
2024-10-31 14:35:05 -0300

    Exemplo de permission no app genres

--- app/settings.py
+++ app/settings.py
@@ -139,6 +139,6 @@ REST_FRAMEWORK = {
 }
 
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=5),
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=30),
     "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
 }
